004f44a406cd311a655504a92ae84809ab96f2a6
DannyCroDev <danielrajic26@outlook.com>
2024-01-14 14:39:28 +0100

    styling dynamic fix, sessionStorage 2.0, JSon parsing podataka

--- Projekt/JS/mainTable.js
+++ Projekt/JS/mainTable.js
@@ -1,3 +1,24 @@
 import { readTeamAdmin } from "./database.js";
 
-const tablicaRezultata = document.getElementById("tablicaRezultata");
+document.addEventListener("DOMContentLoaded", () => {
+    loadStandings();
+})
+
+function loadStandings(){
+    const tablicaRezultata = document.getElementById("tablicaRezultata");
+
+    const standingsData = JSON.parse(localStorage.getItem('standingsData')) || [];
+
+    standingsData.forEach((team, index) => {
+
+        if (index > 0) {
+            const noviRed = tablicaRezultata.insertRow(-1);
+
+            noviRed.innerHTML = `
+                <td>${index}</td>
+                <td>${team.team}</td>
+                <td>${team.points}</td>
+            `;
+        }
+    });
+}
